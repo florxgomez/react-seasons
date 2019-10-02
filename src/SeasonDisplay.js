d7e7b2d99a0192b6ff2b0b8131a92afcdb2d7a78
Flor <florxgomez@gmail.com>
2019-10-01 23:21:03 -0300

    adding styles

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,48 @@
 import React from "react";
+import './SeasonDisplay.css';
 
-const SeasonDisplay = () => {
-  return <div>Seasons</div>;
+const seasonConfig = {
+    summer : {
+        textDisplay: "Let's hit the beach!",
+        iconName : 'sun'
+    },
+    winter: {
+        textDisplay: "Burr it's cold!",
+        iconName : 'snowflake'
+    },
+    spring: {
+        textDisplay: 'Look a those lovely flowers',
+        iconName: 'certificate'
+
+    },
+    autumn: {
+        textDisplay: 'Leaves everywhere',
+        iconName: 'leaf'
+    }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 1 && month < 5) {
+    return lat < 0 ? "autumn" : "spring";
+  } else if (month > 4 && month < 8) {
+    return lat < 0 ? "winter" : "summer";
+  } else if (month > 7 && month < 11){
+      return lat < 0 ? 'spring' : 'autumn'
+  } else if((month >= 0 && month < 2) || month === 11){
+      return lat < 0 ? 'summer' : 'winter'
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {textDisplay, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon huge icon-left`} />
+      <h1>{textDisplay}</h1>
+      <i className={`${iconName} icon huge icon-right`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      errorMessage: "",
-    };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -32,7 +25,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading...</div>;
   }
